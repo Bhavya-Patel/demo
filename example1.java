cfad17f66859d9319bcc8e180f7b155277f195ff
Bhavya <bhavyapatel2552@gmail.com>
2020-08-01 14:23:31 +0530

    Commiting in JavaAnother

---
+++ example1.java
@@ -0,0 +1,7 @@
+public class example1{
+	public static void main(String[] args) {
+		System.out.println("welcome");
+		System.out.println("how are you ");
+		System.out.println("You are in another file created by JavaAnother");
+	}
+}
